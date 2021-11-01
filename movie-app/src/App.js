1744eb381e031056444795e2e4aebeec44ff7433
26seung <yousi7@naver.com>
2021-11-01 23:57:31 +0900

    1

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello!!</h1>
-    </div>
-  );
+class App extends React.Component {
+  getMovies = async () => {
+    const movieApi = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    console.log(movieApi.data.data.movies);
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello!!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
